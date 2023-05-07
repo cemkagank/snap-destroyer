f9eb71e56295a28ebcf28a0d62ee1b21fb5907ed
Cem Kağan Kayılı <cemkagankayili@hotmail.com>
2023-05-07 13:41:01 +0300

    fixed remove order problem

--- sayanora.sh
+++ sayanora.sh
@@ -8,6 +8,15 @@ do
     sudo snap remove --purge $s
 done
 
+for s in $snaps
+do 
+    sudo snap remove --purge $s
+done
+
+sudo snap remove --purge bare
+sudo snap remove --purge snapd
+
+
 sudo apt remove --autoremove snapd
 
 sudo mv nosnap.pref /etc/apt/preferences.d/nosnap.pref
